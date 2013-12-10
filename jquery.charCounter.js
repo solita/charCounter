dbc106ca79510595cfdcfd7a2dcdb28632c98554
Antti-Jussi Kovalainen <ajk@ajk.fi>
2013-12-10 11:03:16 +0200

    Optimize and simplify

--- jquery.charCounter.js
+++ jquery.charCounter.js
@@ -1,5 +1,6 @@
 // jQuery Character Counter - version 0.2
 // by Juhana Harmanen (harmia)
+
 ;(function($) {
 
    $.fn.counter = function(options){
@@ -29,55 +30,60 @@
       };
 
       function calculate(obj){
-         var count = $(obj).val().length;
+         var $obj = $(obj);
+         var value = $obj.val();
+         var count = value.length;
+         var available = options.allowed - count;
 
          if (!options.allowOverflow && count > options.allowed) {
-            $(obj).val($(obj).val().substring(0, options.allowed));
+            $obj.val(value.substring(0, options.allowed));
          }
 
          if (options.disableButton) {
             disableButton(count);
          }
 
-
-
-         var available = options.allowed - count;
-
          if (options.direction == 'up') {
-            $(obj).next().html(count + '/' + options.allowed + options.counterText);
+            $obj.next().html(count + '/' + options.allowed + options.counterText);
          } else if (options.direction == 'down') {
-            $(obj).next().html(available + '/' + options.allowed + options.counterText);
+            $obj.next().html(available + '/' + options.allowed + options.counterText);
          } else {
-            $(obj).next().html(available + options.counterText );
+            $obj.next().html(available + options.counterText );
          }
       };
 
       this.each(function() {
-         $(this).after('<div id="'+ this.id +'Counter" class="' + options.css + '">'+ options.counterText +'</div>');
+         var $this = $(this);
+         var value = $this.val();
+         var len = value.length;
+
+         $this.after('<div id="'+ this.id +'Counter" class="' + options.css + '">'+ options.counterText +'</div>');
 
          calculate(this);
 
-         $(this).bind("keyup change", function(){
+         $this.bind("keyup change", function(){
             calculate(this);
 
-            if ($(this).val().length > (options.allowed - options.warning) && options.onWarning !== undefined) {
+            if (len > (options.allowed - options.warning) && options.onWarning !== undefined) {
                options.onWarning(this);
             }
-            if ($(this).val().length <= (options.allowed - options.warning) && options.onWarningOff !== undefined) {
+
+            if (len <= (options.allowed - options.warning) && options.onWarningOff !== undefined) {
                options.onWarningOff(this);
             }
 
-            if ($(this).val().length > options.allowed && options.onDisallow !== undefined) {
+            if (len > options.allowed && options.onDisallow !== undefined) {
                options.onDisallow(this);
             }
-            if ($(this).val().length <= options.allowed && options.onAllow !== undefined) {
+
+            if (len <= options.allowed && options.onAllow !== undefined) {
                options.onAllow(this);
             }
          });
 
-         $(this).keypress(function(event) {
-            //  if allowed length is reached, allow only modify actions
+         $this.keypress(function(event) {
             if (!options.allowOverflow && $(this).val().length >= options.allowed) {
+               // block anything but text modify actions
                event = ( event ) ? event : window.event;
                var charCode = ( event.which ) ? event.which : event.keyCode
                return charCode == 8 || charCode == 46 || (charCode >= 33 && charCode <= 40)
